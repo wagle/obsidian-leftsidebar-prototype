faadbf4366d9945832332c932675189db146740d
Perry Wagle <wagle@mac.com>
2023-01-05 03:45:00 -0800

    try getIcon

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,9 @@ export default class ExamplePlugin extends Plugin {
 		this.addRibbonIcon("dice", "Activate view", () => {
 			this.activateView();
 		});
+		// this.app.workspace.getLeftLeaf(false).setViewState({
+		// 	type: 'my-view-type',
+		// });
 	}
 
 	async onunload() {
@@ -29,4 +32,8 @@ export default class ExamplePlugin extends Plugin {
 			this.app.workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE)[0]
 		);
 	}
+	getIcon(): string {
+		return "dice";
+	}
+
 }
